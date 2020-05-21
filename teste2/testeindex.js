8cd763d0d88168bd9b3545e9826e2fbc0e2eaf3c
Henrique da Silva Alves <henriquehdsa@gmail.com>
2020-05-21 15:24:58 -0300

    teste22

--- teste2/testeindex.js
+++ teste2/testeindex.js
@@ -1,5 +1,11 @@
-const arrayIdade = [32, 34, 36, 33, 30];
+// const arrayIdade = [32, 34, 36, 33, 30];
 
-const pegaNome = arrayIdade.map((idade, index, tudo) =>{
-    console.log(`${index}-${idade}`);
-});
+// const pegaNome = arrayIdade.map((idade, index, tudo) =>{
+//     console.log(`${index}-${idade}`);
+// });
+
+const soma = function(A, B){
+    return console.log(A + B);
+}
+
+soma(30, 40);
